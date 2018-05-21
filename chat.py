02b8ca4e01b5d5a187e668b6d71a07b6f2176b26
Rintaro Hasegawa <rintaro.hasegawa@gmail.com>
2018-05-21 09:04:37 +0000

    ホームページに入力されたtwitterアカウントのタイムラインを自動で取得し、データベースに保存するプログラムを書きました

--- chat.py
+++ chat.py
@@ -1,32 +1,38 @@
+import get_timelines
 from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 from tinydb import TinyDB, Query
 db = TinyDB('chat_db.json')
 db.purge()
+
+
 @app.route("/")
 #@app.route("/index")
 def index():
-    
-    return render_template('index.html')
+
+  return render_template('index.html')
+
 
 @app.route("/registration")
 def add():
-    db.insert({
-        'adress': request.args.get('Adress'),
-        "area":request.args.get("area"),
-        'ID': request.args.get('ID'),
-        "interesting":request.args.getlist('events')
-    })
+  db.insert({
+      'adress': request.args.get('Adress'),
+      "area": request.args.get("area"),
+      'ID': request.args.get('ID'),
+      "interesting": request.args.getlist('events')
+  })
+  get_timelines.get(request.args.get('ID'))
 
-    return render_template('registercompleted.html', posts = db.all())
+  return render_template('registercompleted.html', posts=db.all())
 
 #@app.route("/reset")
-#def reset():
+# def reset():
 #    if db is not None:
 #        db.purge()
 #    db.insert({'adress': 'Pecha','area':'Welcome','ID':'id'})
 #    return index()
 
+
 if __name__ == "__main__":
-    app.run(debug = True, port=5000, host="0.0.0.0")
+  app.run(debug=True, port=5000, host="0.0.0.0")
