88c3979c958ab8d785ec918f9f4da9df29aef459
falfal07 <mokuri3y.kaki8y.2016@gmail.com>
2018-05-27 07:48:27 +0000

    update location

--- sort_by_distance.py
+++ sort_by_distance.py
@@ -2,35 +2,44 @@
 
 from urllib import request,parse
 import json
+import get_location as gl
 
-places = [{'title': '北海道', 'lat': 22, 'lng': 139}, {'title': '沖縄', 'lat': 23, 'lng': 140}, {'title': '東京', 'lat': 21,'lng': 150}]
-yourPlace = {'lat': 40, 'lng': 140}
+#places = [{'title': '北海道', 'latitude': 22, 'longitude': 139}, {'title': '沖縄', 'latitude': 23, 'longitude': 140}, {'title': '東京', 'latitude': 21,'longitude': 150}]
 
-url = 'http://vldb.gsi.go.jp/sokuchi/surveycalc/surveycalc/bl2st_calc.pl?'
 
-distance = {}               # 距離の辞書
+def get(places,postcode):
 
-for i in range(0, len(places)):
-    param = {
-        'outputType': 'json',
-        'ellipsoid':  'bessel',
-        'latitude1': yourPlace['lat'],            # 使用者の緯度
-        'longitude1': yourPlace['lng'],          # 使用者の経度
-        'latitude2': places[i]['lat'],            # 比較地点の緯度
-        'longitude2': places[i]['lng'],          # 比較地点の経度
-    }
-    paramStr = parse.urlencode(param)
-    readObj = request.urlopen(url + paramStr)
-    res = readObj.read().decode()
-    data =json.loads(res)
-#distance[str(places[i]['title'])] = data['OutputData']['geoLength']    # ２点間の距離(KM)をtitleと共に追加
-    places[i]['distance'] = data['OutputData']['geoLength']
-#print(distance)
+    geocoding = gl.get(postcode)
+    yourPlace = {'lat': geocoding['lat'], 'lng': geocoding['lng']}
+    print('aho')
+    url = 'http://vldb.gsi.go.jp/sokuchi/surveycalc/surveycalc/bl2st_calc.pl?'
 
-places.sort(key=lambda x: x['distance'])
 
-for k in range(0, len(places)):
-    print(places[k]['title'])
-    print(places[k]['distance'])
-    print()
+    for i in range(0, len(places)):
+        if places[i]['latitude'] == '':
+            continue
+        else:
+            param = {
+                'outputType': 'json',
+                'ellipsoid':  'bessel',
+                'latitude1': yourPlace['lat'],            # 使用者の緯度
+                'longitude1': yourPlace['lng'],          # 使用者の経度
+                'latitude2': places[i]['latitude'],            # 比較地点の緯度
+                'longitude2': places[i]['longitude'],          # 比較地点の経度
+            }
+            paramStr = parse.urlencode(param)
+            readObj = request.urlopen(url + paramStr)
+            res = readObj.read().decode()
+            data_distance =json.loads(res)
+            print(data_distance)
+            places[i]['distance'] = data_distance['OutputData']['geoLength']      # ２点間の距離(M)を追加
+                #print(distance)
 
+    places.sort(key=lambda x: x['distance'])
+
+    for k in range(0, len(places)):
+        print(places[k]['title'])
+        print(places[k]['distance'])
+        print()
+
+#get(places)
